1ccb092d13adf4df6df796d9e3661cdbe9c5426f
rodrigo.baggio.si@gmail.com <Baggio25>
2024-11-19 07:31:51 -0300

    Capturando o evento de input

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//Seleciona os elementos do formulário
+const amount = document.getElementById("amount");
+
+//Fica observando toda vez que entrar algum conteúdo no input
+amount.oninput = () => {
+  
+}
+
