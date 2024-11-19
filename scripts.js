39aa62825c42c0d85f086588cf052aee6d9abedf
rodrigo.baggio.si@gmail.com <Baggio25>
2024-11-19 07:34:35 -0300

    Aceitando somente números no valor

--- scripts.js
+++ scripts.js
@@ -3,6 +3,11 @@ const amount = document.getElementById("amount");
 
 //Fica observando toda vez que entrar algum conteúdo no input
 amount.oninput = () => {
-  
+
+  //Remove as letras, não permitindo informar no campo 
+  let value = amount.value.replace(/\D/g, "");
+  amount.value = value; 
+
+
 }
 
